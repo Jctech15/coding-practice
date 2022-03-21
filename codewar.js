f12b823097f52335e97bc1be0f53d0acf75607cd
Janet <jyctech15@gmail.com>
2022-03-21 12:18:48 -0700

    split string to find max and min

--- codewar.js
+++ codewar.js
@@ -26,12 +26,12 @@ console.log(disemvowel("This website is for losers LOL!"));
  */
 
 //kyu 6 Create Phone Number
-function createPhoneNumber(numbers) {
-  //   let first = numbers.slice(0, 3);
-  //   let second = numbers.slice(3, 6);
-  //   let third = numbers.slice(-4);
+/* function createPhoneNumber(numbers) {
+     let first = numbers.slice(0, 3);
+     let second = numbers.slice(3, 6);
+     let third = numbers.slice(-4);
 
-  //   return `(${first.join("")}) ${second.join("")}-${third.join("")}`;
+     return `(${first.join("")}) ${second.join("")}-${third.join("")}`;
   let result = "";
   numbers.forEach((number, index) => {
     switch (index) {
@@ -52,4 +52,36 @@ function createPhoneNumber(numbers) {
 }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
-//"(123) 456-7890"
+//"(123) 456-7890" */
+
+//kyu 6 find the odd int
+/* function findOdd(A) {
+  let counter = 0;
+  let newA = [];
+  A.forEach((num, i) => {
+    if (num[i] === num[i + 1]) {
+      counter++;
+    }
+  });
+  if (counter % 2 === 0) {
+    console.log(num[i]);
+  }
+}
+
+let a = [1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1];
+findOdd(a); //4 */
+
+//kyu 7 Highest and Lowest
+function highAndLow(numbers) {
+  // let splitNum = numbers.split(" ");
+  // let arrayNum = [];
+  // splitNum.forEach((element) => {
+  //   arrayNum.push(Number(element));
+  // });
+  // let max = Math.max(...arrayNum);
+  // let min = Math.min(...arrayNum);
+  // return `${max} ${min}`;
+  numbers = numbers.split(" ");
+  console.log(`${Math.max(...numbers)} ${Math.min(...numbers)}`);
+}
+highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");

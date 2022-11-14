e13de991092d2914f825119f6daf78ce1d1e1c7b
Jctech15 <jyctech15@gmail.com>
2022-11-14 14:03:09 -0800

    array methods review
    filter
    map
    shift
    sort

--- codewar.js
+++ codewar.js
@@ -1018,14 +1018,44 @@ shuffleIt([1, 2, 3, 4, 5], [1, 2], [3, 4]); */
 // //You're writing code to control your town's traffic lights.
 // //You need a function to handle each change from green, to yellow, to red, and then to green again.
 
-//kyu 8 Get the mean of an array
-function getAverage(marks) {
-  //TODO : calculate the downward rounded average of the marks array
-  let sum = 0;
-  marks.forEach((i) => {
-    sum += i;
+// //kyu 8 Get the mean of an array
+// function getAverage(marks) {
+//   //TODO : calculate the downward rounded average of the marks array
+//   let sum = 0;
+//   marks.forEach((i) => {
+//     sum += i;
+//   });
+//   console.log(Math.floor(sum / marks.length));
+// }
+
+// getAverage([1, 1, 1, 1, 1, 1, 1, 2]);
+
+//kyu 6 Sort theh odd
+function sortArray(array) {
+  let oddArray = array
+    .filter((num) => Math.abs(num) % 2 === 1)
+    .sort((a, b) => a - b);
+
+  let sortedArray = [];
+  array.forEach((num) => {
+    if (Math.abs(num) % 2 === 1) {
+      sortedArray.push(oddArray.shift());
+    } else {
+      sortedArray.push(num);
+    }
   });
-  console.log(Math.floor(sum / marks.length));
+  console.log(sortedArray);
 }
 
-getAverage([1, 1, 1, 1, 1, 1, 1, 2]);
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a, b) => a - b);
+  //[3,5]
+  return array.map((x) => (x % 2 ? odd.shift() : x));
+  //[5, 8, 6, 3, 4];
+}
+
+// sortArray([
+//   -41, -44, 5, 9, 11, -30, -38, 13, -3, 21, 10, -19, -46, -10, 16, -4, -45, -14,
+//   -10, 10, 31, 31, 33, -25, -2, -23, 38, 16, -37,
+// ]); //[ -45,   -44,  -41,  -37,  -25,  -30,  -38,  -23,  -19,  -3,  10,  5,  -46,  -10,  16,  -4,  9,  -14,  -10,  10,  11,  13,  21,  31,  -2,  31,  38,  16,  33 ]
+//[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
